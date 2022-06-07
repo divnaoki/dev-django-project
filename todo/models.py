87f090a9eaf0742d5d4322ffab8893d7ccfd4354
divnaoki <naokiimai1@gmail.com>
2022-06-07 09:27:03 +0900

    create todo schama

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Todo(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.TextField(blank=True, null=True)
+  create_time = models.DateTimeField(default=timezone.now)
+  update_time = models.DateTimeField(default=timezone.now)
+  limit_date = models.DateField(null=False)
+  status = models.BooleanField(default=False)
